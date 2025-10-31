25cec10c5fdeb2a034b7d60fa94eda6b05a25133
caiolucasxz55 <caiolucasxz55@gmail.com>
2025-10-31 09:59:48 -0300

    refactor: clean up comments in prisma.ts for clarity

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-// Singleton para evitar múltiplas instâncias do Prisma Client
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
@@ -13,18 +13,18 @@ if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
 }
 
-// Função para conectar ao banco
+
 export const connectDatabase = async () => {
   try {
     await prisma.$connect();
     console.log("✅ Conectado ao banco de dados com sucesso.");
   } catch (error) {
     console.error("❌ Erro ao conectar ao banco de dados:", error);
-    process.exit(1); // Encerra o processo se não conseguir conectar
+    process.exit(1); 
   }
 };
 
-// Função para desconectar do banco
+
 export const disconnectDatabase = async () => {
   try {
     await prisma.$disconnect();
