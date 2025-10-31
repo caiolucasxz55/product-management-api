c4c444dbf8ae4e4d479dd576f80a637d2676bc4c
caiolucasxz55 <caiolucasxz55@gmail.com>
2025-10-31 08:45:14 -0300

    fix: update CORS configuration to allow requests from localhost only

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000", 
-      "https://seu-front-no-render.vercel.app", 
+      "http://localhost:3001", 
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
