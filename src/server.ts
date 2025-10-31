1b0f80c366af5616e17e684780c7113abb82cfd2
caiolucasxz55 <caiolucasxz55@gmail.com>
2025-10-31 01:39:10 -0300

    fix: remove unused express import from server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from "express";
+
 import dotenv from "dotenv";
 import { connectDatabase, prisma } from "./config/prisma";
 import { applySecurityConfig } from "./config/securityConfig";
